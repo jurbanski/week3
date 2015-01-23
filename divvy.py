7253cebec3836c0f7b2ecf3033250af7fdd169c0
Joseph Urbanski <jurbanski@ci.uchicago.edu>
2015-01-23 14:45:50 -0600

    Initial commit, removing provided code.

--- divvy.py
+++ divvy.py
@@ -1,21 +1,7 @@
-# Divvy Bikes
-#
-# Here's an example of how to retrieve the list of Divvy bike stations:
+#!/usr/local/bin/python3
+# 2015-01-23
+# Joseph Urbanski
+# MPCS 50101
+# Week 3 homework
+# divvy.py
 
-import json
-from urllib.request import urlopen
-
-webservice_url = "http://www.divvybikes.com/stations/json"
-data = urlopen(webservice_url).read().decode("utf8")
-result = json.loads(data)
-stations = result['stationBeanList']
-print(stations)
-
-# The Young building has the following latitude and longitude: 41.793414,-87.600915.
-# To measure surface distance, you can treat latitudes and longitudes like x and y coordinates, and calculate distance between locations with the usual Euclidean distance formula.
-
-# 1. Modify the code above to display the station name and number of available bikes for the station closest to Young.
-
-# You will likely want to consult the JSON stream from Divvy
-
-# - http://www.divvybikes.com/stations/json
